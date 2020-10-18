5824dd5874847bee5084adb1fbff177a70091ef1
Gabe Fierro <gtfierro@cs.berkeley.edu>
2020-10-17 23:14:21 -0600

    use notify to subscribe to triple changes

--- docker/mortar-pg/setup.sql
+++ docker/mortar-pg/setup.sql
@@ -65,3 +65,31 @@ CREATE VIEW latest_triples AS
     SELECT triples.source, s, p, o 
     FROM triples
     LEFT JOIN lts USING(source, origin, time);
+
+
+-- for notification when triples changes
+-- from https://citizen428.net/blog/asynchronous-notifications-in-postgres/access
+CREATE OR REPLACE FUNCTION notify_event() RETURNS TRIGGER AS $$
+  DECLARE
+    record RECORD;
+    payload JSON;
+  BEGIN
+    IF (TG_OP = 'DELETE') THEN
+      record = OLD;
+    ELSE
+      record = NEW;
+    END IF;
+
+    payload = json_build_object('table', TG_TABLE_NAME,
+                                'action', TG_OP,
+                                'data', row_to_json(record));
+
+    PERFORM pg_notify('events', payload::text);
+
+    RETURN NULL;
+  END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER notify_triple_change
+AFTER INSERT OR UPDATE OR DELETE ON triples
+  FOR EACH ROW EXECUTE PROCEDURE notify_event();
