cd9301420bf6de17ad30d6c592a39d28e9e731e4
Gabe Fierro <gtfierro@cs.berkeley.edu>
2020-12-16 10:32:00 -0800

    add timeouts file

---
+++ internal/config/timeouts.go
@@ -0,0 +1,11 @@
+package config
+
+import (
+	"time"
+)
+
+// DataReadTimeout is the maximum allowed time for a data query to take before it is cancelled
+const DataReadTimeout = time.Duration(15 * time.Minute)
+
+// DataWriteTimeout is the maximum allowed time for a data insertion to take before it is cancelled
+const DataWriteTimeout = time.Duration(15 * time.Minute)
